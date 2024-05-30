066dfd001bee28a8fa7cb5596b1d866aaf3969a5
choral-riff <60531846+choral-riff@users.noreply.github.com>
2024-05-30 02:57:58 -0400

    Create 2_is_anagram.cpp

---
+++ 2_is_anagram.cpp
@@ -0,0 +1,36 @@
+/*
+Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+
+An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+Example 1:
+
+Input: s = "racecar", t = "carrace"
+
+Output: true
+Example 2:
+
+Input: s = "jar", t = "jam"
+
+Output: false
+Constraints:
+
+s and t consist of lowercase English letters.
+*/
+
+#include <algorithm>
+
+class Solution {
+public:
+   
+    bool isAnagram(string s, string t) {
+        sort (s.begin(), s.end());
+        sort (t.begin(), t.end());
+        if (s == t){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
